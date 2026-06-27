7d8192eacf2b2e3c3386bc08a9781242e1fcfdd7
kotteeswaran12 <kotteeswaran2005@gmail.com>
2026-06-27 08:51:25 +0530

    "Day-1"

--- TwoPointer/MoveZero.java
+++ TwoPointer/MoveZero.java
@@ -1,6 +1,38 @@
+
+import java.util.Arrays;
+
 public class MoveZero {
 
-    public static void main(String[] args) { 
-    System.out.println("Hello");
-}
+    public static void main(String[] args) {
+
+        int arr[] = {4, 2, 4, 0, 0, 3, 0, 5, 1, 0};
+
+        System.out.println(Arrays.toString(moveZero1(arr)));
+    }
+
+    public static int[] moveZero1(int[] arr) {
+
+        int i = 0;
+        int j = i + 1;
+
+        while (j < arr.length) {
+            System.out.println("i : " + arr[i] + " , " + " J : " + arr[j]);
+
+            if (arr[i] == 0 && arr[j] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+            } else if (arr[i] != 0) {
+                i++;
+            }
+
+            j++;
+
+        }
+
+        return arr;
+
+    }
+
 }
